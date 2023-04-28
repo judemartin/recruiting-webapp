7549337606c682c0f6882a60de8b1b81e680c607
Jude Martin <judemartin.mj@gmail.com>
2023-04-28 14:28:23 -0500

    Added prettier

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,25 +1,22 @@
-import Klasses from "./Klasses";
-import Skills from "./Skills";
-import Attributes from "./Attributes";
+import Klasses from './Klasses';
+import Skills from './Skills';
+import Attributes from './Attributes';
 
 const Character = ({ attributes, updateCharacter, skills, index }) => {
-    return (
-        <div>
-            <h1 style={{ marginTop: 40 }}>Character #{index}</h1>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <Attributes
-                    attributes={attributes}
-                    updateCharacter={updateCharacter}
-                />
-                <Klasses characterAttributes={attributes} />
-                <Skills
-                    attributes={attributes}
-                    skills={skills}
-                    updateCharacter={updateCharacter}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1 style={{ marginTop: 40 }}>Character #{index}</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Attributes attributes={attributes} updateCharacter={updateCharacter} />
+        <Klasses characterAttributes={attributes} />
+        <Skills
+          attributes={attributes}
+          skills={skills}
+          updateCharacter={updateCharacter}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Character;
--- src/components/SkillsDropDown.js
+++ src/components/SkillsDropDown.js
@@ -1,66 +1,83 @@
-import React, {useEffect} from 'react';
-import {SKILL_LIST} from "../consts";
+import React, { useEffect, useState } from 'react';
+import { SKILL_LIST } from '../consts';
 
 const SkillsDropDown = ({ skills }) => {
-    const skillsDropdownNames= SKILL_LIST.map(skill => skill.name);
-    const [difficultyChosen, setDifficultyChosen] = React.useState(0);
-    const [randomNumber, setRandomNumber] = React.useState(0);
+  const skillsDropdownNames = SKILL_LIST.map((skill) => skill.name);
+  const [difficultyChosen, setDifficultyChosen] = React.useState(0);
+  const [randomNumber, setRandomNumber] = React.useState(0);
 
-    //const allSkills =
+  const [allSkills, setAllSkills] = useState(skills);
+  const [skill, setSkill] = React.useState(
+    allSkills.find((skill) => skill.name === skillsDropdownNames[0])
+  );
+  const [
+    isChooseCharacterSkillSuccessfull,
+    setIsChooseCharacterSkillSuccessfull
+  ] = React.useState(false);
+  const [computedNumber, setComputedNumber] = React.useState(0);
 
-    const [skill, setSkill] = React.useState(skills.find(skill => skill.name === skillsDropdownNames[0]));
-    const [isChooseCharacterSkillSuccessfull, setIsChooseCharacterSkillSuccessfull] = React.useState(false);
-    const [computedNumber, setComputedNumber] = React.useState(0);
+  useEffect(() => {
+    setAllSkills(skills);
+  }, [skills]);
 
+  useEffect(() => {
+    const skill = allSkills.find((skill) => skill.name === skill.name);
+    console.log({ skill });
+    setSkill(skill);
+  }, [allSkills]);
 
-    useEffect(() => {
-        console.log({ skills });
-        setSkill(skills.find(skill => skill.name === skill.name));
-    }, [skills]);
+  const handleRoll = () => {
+    const randomNumber = Math.floor(Math.random() * 20) + 1;
+    setRandomNumber(randomNumber);
+    const doesMeetSelectionCriteria =
+      randomNumber + skill.value >= difficultyChosen;
+    setComputedNumber(randomNumber + skill.value);
+    setIsChooseCharacterSkillSuccessfull(doesMeetSelectionCriteria);
+  };
 
-    const handleRoll = () =>  {
-        const randomNumber = Math.floor(Math.random() * 20) + 1;
-        setRandomNumber(randomNumber);
-        const doesMeetSelectionCriteria = randomNumber + skill.value >= difficultyChosen;
-        setComputedNumber(randomNumber + skill.value);
-        setIsChooseCharacterSkillSuccessfull(doesMeetSelectionCriteria);
+  const handleSkillSelection = (e) => {
+    const skillName = e.target.value;
+    const skill = allSkills.find((skill) => skill.name === skillName);
+    console.log({ skill });
+    setSkill(skill);
+  };
+
+  const handleDifficultyChosenInputChange = (e) => {
+    const value = +e.target.value;
+    if (!isNaN(value)) {
+      setDifficultyChosen(+e.target.value);
     }
+  };
 
-    const handleSkillSelection = (e) => {
-        const skillName = e.target.value;
-        const skill = skills.find(skill => skill.name === skillName);
-        console.log({ skill });
-        setSkill(skill);
-    }
+  return (
+    <div>
+      <h1>SkillsDropDown</h1>
+      <select onChange={(e) => handleSkillSelection(e)}>
+        {skillsDropdownNames.map((skill) => (
+          <option key={skill}>{skill}</option>
+        ))}
+      </select>
+      <input
+        type={'text'}
+        value={difficultyChosen}
+        onChange={(e) => handleDifficultyChosenInputChange(e)}
+      />
+      <button onClick={handleRoll}>Roll</button>
+      {randomNumber}
 
-    return (
-        <div>
-            <h1>SkillsDropDown</h1>
-            <select onChange={(e) => handleSkillSelection(e) }>
-                {skillsDropdownNames.map(skill => <option key={skill}>{skill}</option>)}
-            </select>
-            <input type={"text"} value={difficultyChosen} onChange={(e) => {
-                const value = +e.target.value;
-                if(typeof  value === "number")
-                    setDifficultyChosen(+e.target.value)
-                else
-                    setDifficultyChosen(0)
-            }}/>
+      {skill ? (
+        <>
+          {skill.name} {skill.value}
+        </>
+      ) : null}
 
-            <button onClick={handleRoll}>Roll</button>
-            {randomNumber}
-
-            {
-                skill ? <> {skill.name} {skill.value} </> : null
-
-
-            }
-
-            {
-                isChooseCharacterSkillSuccessfull ? <>skill check is successful {computedNumber}  </> : <>skill check is a failure {computedNumber}</>
-            }
-        </div>
-    );
+      {isChooseCharacterSkillSuccessfull ? (
+        <>skill check is successful {computedNumber} </>
+      ) : (
+        <>skill check is a failure {computedNumber}</>
+      )}
+    </div>
+  );
 };
 
 export default SkillsDropDown;
